5c748ff400e615178a227706eba7f019bedf2e80
Luis Rodriguez <luiserr1394@gmail.com>
2023-06-17 13:18:38 -0300

    Melhoras na Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,47 @@
 <template>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Estacionamento</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+         <router-link   class="nav-link active" to="/">Home</router-link> 
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+        <router-link class="nav-link" to="/about">About</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+           <router-link class="nav-link" to="/listar-marcas">Marcas</router-link>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+       
+        <li class="nav-item">
+         <router-link class="nav-link" to="/listar-condutores">Condutores</router-link> 
         </li>
+      
+        <li class="nav-item">
+          <router-link class="nav-link" to="/listar-configuracoes">Configuracoes</router-link>  
+        </li>
+     
+        <li class="nav-item">
+          <router-link class="nav-link" to="/listar-modelos">Modelos</router-link>  
+        </li>
+       
+        <li class="nav-item">
+         <router-link class="nav-link" to="/listar-movimentacoes">Movimentacoes</router-link>  
+        </li>
+
+        <li class="nav-item">
+         <router-link class="nav-link" to ="/listar-veiculos">Veiculos</router-link>
+        </li>
+        
       </ul>
     </div>
   </div>
 </nav>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/listar-marcas">Marcas</router-link> |
-    <router-link to="/listar-condutores">Condutores</router-link> |
-    <router-link to="/listar-configuracoes">Configuracoes</router-link>  |
-    <router-link to="/listar-modelos">Modelos</router-link>  |
-    <router-link to="/listar-movimentacoes">Movimentacoes</router-link>  |
-    <router-link to ="/listar-veiculos">Veiculos</router-link>
-  </nav>
+
   <router-view/>
 </template>
 
